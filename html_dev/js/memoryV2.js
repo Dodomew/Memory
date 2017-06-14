d656e3589d9493a0e71cffee5f59ae617117156c
Doortje Spanjerberg <dodomew@gmail.com>
2017-06-14 16:51:07 +0200

    rewrote turning tiles function, click function, check which tile was clicked function

--- html_dev/js/memoryV2.js
+++ html_dev/js/memoryV2.js
@@ -3,6 +3,7 @@
   var tileNumberContainer = [];
   var tileObjectsContainer = [];
   var contentContainer = document.getElementById("content-container");
+  var clickedTilesContainer = [];
 
   let userInputButton = document.getElementById("userInputButton");
   userInputButton.addEventListener('click', setAmountOfTiles, false);
@@ -26,16 +27,16 @@
 
       contentContainer.appendChild(this.divTile);
 
-      let innerTileContainerFront = document.createElement("div");
-      innerTileContainerFront.className = "front";
+      this.innerTileContainerFront = document.createElement("div");
+      this.innerTileContainerFront.className = "front";
 
-      let innerTileContainerBack = document.createElement("div");
-      innerTileContainerBack.className = "back";
+      this.innerTileContainerBack = document.createElement("div");
+      this.innerTileContainerBack.className = "back";
 
-      this.divTile.appendChild(innerTileContainerFront);
-      this.divTile.appendChild(innerTileContainerBack);
+      this.divTile.appendChild(this.innerTileContainerFront);
+      this.divTile.appendChild(this.innerTileContainerBack);
 
-      this.divTile.addEventListener('click', whichTileIsClicked, false);
+      this.divTile.addEventListener('click', didUserClickTile, false);
     }
   }
 
@@ -74,20 +75,7 @@
     while (tileNumberContainer.length > 0);
   }
 
-/*
-  function turnTileFaceUp(event)
-  {
-    let e = window.event || event;
-    let target = e.target || e.srcElement;
-
-    if(target.className == "front")
-    {
-      console.log("yay");
-    }
-  }
-*/
-
-  function whichTileIsClicked(event)
+  function didUserClickTile(event)
   {
     let e = event.currentTarget;
     let targetId = e.id || e.srcElement.id;
@@ -95,19 +83,88 @@
 
     if(targetClass.indexOf("tile-container") >= 0)
     {
+      whichTileIsClicked(targetId);
+    }
+  }
+
+  function whichTileIsClicked(clickedTileId)
+  {
+    if(noMoreThanTwoTilesFaceUp() <= 1)
+    {
       for (let i = 0; i < tileObjectsContainer.length; i++)
       {
-        if(targetId === tileObjectsContainer[i].divTile.id)
+        if(clickedTileId === tileObjectsContainer[i].divTile.id && tileObjectsContainer[i].tileState == TileState.FACE_DOWN)
         {
-          console.log("e");
+          turnTileFaceUp(tileObjectsContainer[i]);
+
+          if(clickedTilesContainer.length == 2)
+          {
+            compareTwoTiles(clickedTilesContainer);
+          }
         }
       }
     }
   }
 
-  function turnTileFaceUp()
+  function turnTileFaceUp(clickedTile)
+  {
+    clickedTile.divTile.classList.add("flipped");
+    clickedTile.innerTileContainerBack.innerHTML = clickedTile.hiddenNumber;
+    clickedTile.tileState == TileState.FACE_UP;
+    clickedTilesContainer.push(clickedTile);
+  }
+
+  function compareTwoTiles(arrayOfClickedTiles)
+  {
+    let tileToCompareTo = arrayOfClickedTiles[0];
+    let tileToBeCompared = arrayOfClickedTiles[1];
+
+    if(tileToCompareTo.hiddenNumber == tileToBeCompared.hiddenNumber)
+    {
+      setTimeout(function ()
+      {
+        console.log("yay");
+      },
+      1000);
+    }
+    else
+    {
+      setTimeout(function ()
+      {
+        turnTileFaceDown(tileToCompareTo);
+        turnTileFaceDown(tileToBeCompared);
+        clickedTilesContainer = [];
+      },
+      1000);
+    }
+  }
+
+  function turnTileFaceDown(clickedTile)
+  {
+    clickedTile.divTile.classList.remove("flipped");
+    clickedTile.innerTileContainerBack.innerHTML = "";
+    clickedTile.tileState == TileState.FACE_DOWN;
+    clickedTilesContainer.splice(clickedTile);
+  }
+
+  function removeTileFromGame()
   {
 
+  }
+
+  function noMoreThanTwoTilesFaceUp()
+  {
+    let faceUpCounter = 0;
+
+    for (let i = 0; i < tileObjectsContainer.length; i++)
+    {
+      if(tileObjectsContainer[i].tileState == TileState.FACE_UP)
+      {
+        faceUpCounter++;
+      }
+    }
+
+    return faceUpCounter;
   }
 
   function scaleIn()
